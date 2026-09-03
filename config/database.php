19b43a4d188ccca8601050c2c2e488f99cf6f967
Pyxious <vincealcayde2@gmail.com>
2026-09-03 18:25:36 +0800

    fix: revert emulate prepares for pgsql boolean compatibility and optimize admin layout queries

--- config/database.php
+++ config/database.php
@@ -78,9 +78,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => [
-                \PDO::ATTR_EMULATE_PREPARES => true,
-            ],
         ],
 
         'sqlsrv' => [
